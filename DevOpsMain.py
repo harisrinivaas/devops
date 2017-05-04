0d9af5a91091a275f266678cf265656a132d9119
srinivas.kanduri <harisrinivaas@gmail.com>
2017-05-04 20:36:24 +0530

    uncommented lambda update

--- DevOpsMain.py
+++ DevOpsMain.py
@@ -13,17 +13,17 @@ if __name__ == '__main__':
     changedEMRs = crobj.getChangedEMRs()
     print "EMRs: " , changedEMRs
 
-    # for eachLambda in changedLambdas:
-    #     config = crobj.configReaderObj.getConfiguration(eachLambda)
-    #     # print config
-    #     if LambdaUpdater(config).upload_lambda_fuction():
-    #         print "Lamda Function: %s updated successfully" % config['serviceName']
-    #     else:
-    #         print "FAILED to update Lamda Function: %s" % config['serviceName']
+    for eachLambda in changedLambdas:
+        config = crobj.configReaderObj.getConfiguration(eachLambda)
+        # print config
+        if LambdaUpdater(config).upload_lambda_fuction():
+            print "Lamda Function: %s updated successfully" % config['serviceName']
+        else:
+            print "FAILED to update Lamda Function: %s" % config['serviceName']
 
     for each in changedEMRs:
         changedEMRjobs = crobj.identifyEMRjobs(each)
         print "EMR Cluster:%s Jobs Changed:%s " % (each, changedEMRjobs)
         for eachJobChanged in changedEMRjobs:
             config = crobj.configReaderObj.getEMRJobConfiguration(eachJobChanged['jobName'])
-            print "JobName: %s JobType: %s JobConfig:%s" % (eachJobChanged['jobName'], eachJobChanged['jobType'], config)
+            print "JobName:%s JobType:%s JobConfig:%s" % (eachJobChanged['jobName'], eachJobChanged['jobType'], config)
